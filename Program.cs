17c2d20bd5ce0aa345faef15d3649bc297bdc343
gold leo26 figueredo Cañizales <leonfig26@gmail.com>
2025-05-10 23:27:18 -0400

    v

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace ejemplo
             try
             {
               
-                Application.Run(new Form1());
+                Application.Run(new Login());
             }
             catch (System.NullReferenceException ex)
             {
